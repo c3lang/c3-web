50a4bd8b3e517bf24561b64d9224cd8d8fbcfcfe
onrirr <ulughann@gmail.com>
2024-01-07 17:45:03 +0300

    Update Astro configuration and navbar link

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,11 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import starlight from '@astrojs/starlight';
 
-
-
 // https://astro.build/config
 export default defineConfig({
   site: 'https://onrirr.github.io',
+  base: '/c3-web',
   integrations: [tailwind(), starlight({
     title: "The C3 Handbook"
-  })]
+  })],
 });
