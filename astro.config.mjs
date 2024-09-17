c83abd593a37e17a8d59e03bd75c851690eb77a5
joshringuk@gmail.com <joshringuk@gmail.com>
2024-09-17 15:50:06 +0100

    added redirects to common pages in the old structure corrected links on frontpage made navbar more obvious on hover, not less added discord to navbar rather than dropdown

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,19 @@ import fs from "node:fs"
 // https://astro.build/config
 export default defineConfig({
   site: "https://c3-lang.org",
+  redirects: {
+    '/guide': '/introduction',
+    '/guide/my-first-hello-world': '/getting-started/my-first-hello-world',
+    '/guide/my-first-project': '/getting-started/my-first-project',
+    '/guide/basic-types-and-values': '/getting-started/basic-types-and-values',
+    '/references/development': '/get-involved',
+    '/references': '/introduction/design-goals',
+    '/references/docs/examples': '/language-overview/examples',
+    '/references/getting-started/prebuilt-binaries': '/install-c3/prebuilt-binaries',
+    '/references/getting-started/setup': '/install-c3/compile',
+    '/references/getting-started/primer': '/language-overview/primer',
+    '/references/getting-started/allfeatures': '/language-overview/allfeatures',
+  },
   integrations: [
     tailwind({
       applyBaseStyles: false,
