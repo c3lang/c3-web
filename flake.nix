17b7f92929f13cad46c67ae5cee35d151051dea5
m0tholith <48518572+m0tholith@users.noreply.github.com>
2025-03-05 19:41:34 +0300

    Added flake.nix for any users using Nix or NixOS. (#114)
    * Added flake.nix for any users using Nix or NixOS.

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    { self, ... }@inputs:
+    inputs.flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import inputs.nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            nodejs
+          ];
+        };
+      }
+    );
+}
